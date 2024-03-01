bd311fae982d4cd59e97ce7b5cc47975f1dadd33
Yaroslav Pyrikov <yarko92@rambler.ru>
2024-03-01 16:48:42 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class PhoneBook {
+
+    public int add(String name, String numberPhone) {
+        return 0;
+    }
 }
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,5 +1,16 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class PhoneBookTest {
 
+    @Test
+    public void add() {
+        PhoneBook phoneBook = new PhoneBook();
+        int actualSizePhoneBook = phoneBook.add("Yaroslav", "7-999-999-99-99");
+
+        assertEquals(1, actualSizePhoneBook);
+    }
 }
